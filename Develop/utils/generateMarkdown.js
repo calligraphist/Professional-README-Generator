e378a282543e451eec620744e5355e2a4cdf5b2d
“anar <sehar.uzair23@gmail.com>
2023-03-22 13:50:54 -0300

    license link code added

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -20,15 +20,15 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-// function renderLicenseSection(license) {
-//   if (license!== `no license`) {
-//     return `## [License](#table-of-contents)
-//     This application is under the license:
-//     ${renderLicenseLink(license)}`;
-//   } else {
-//     return ``;
-//   }
-// }
+function renderLicenseSection(license) {
+  if (license!== `no license`) {
+    return `## [License](#table-of-contents)
+    This application is under the license:
+    ${renderLicenseLink(license)}`;
+  } else {
+    return ``;
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
